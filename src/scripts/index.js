a29b10f168561bc301bfe5bd30d2cd8d78f93a84
irfan-fauzi <irfanwintech@yahoo.com>
2021-05-20 20:13:33 +0700

    hardcode list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
-import main from './components-menus/main.js';
 
-document.addEventListener("DOMContentLoaded", main)
 const clickMenu = () => {
   const menuBtn = document.querySelector('.nav__btn');
   const navList = document.querySelector('.nav__list');
