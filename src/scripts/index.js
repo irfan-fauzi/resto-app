ace29d7e1c0773aaf6094a10156b91b4160ecc6e
irfan-fauzi <irfanwintech@yahoo.com>
2021-05-20 15:11:42 +0700

    menu toggle navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,4 +3,12 @@ import '../styles/main.css';
 import main from './components-menus/main.js';
 
 document.addEventListener("DOMContentLoaded", main)
+const clickMenu = () => {
+  const menuBtn = document.querySelector('.nav__btn');
+  const navList = document.querySelector('.nav__list');
+  menuBtn.addEventListener('click', function(){ 
+    navList.classList.toggle('show');
+  })
+}
 
+clickMenu();
